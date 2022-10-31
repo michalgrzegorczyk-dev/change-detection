d296deda119e1b48ba21841c319fe6ccf33ae797
Michal Grzegorczyk <michalgrzegorczyk@michals-macbook-air.home>
2022-10-31 14:59:51 +0100

    cd

--- src/app/child1/child1.component.ts
+++ src/app/child1/child1.component.ts
@@ -1,19 +1,24 @@
-import {Component, ElementRef, DoCheck, ChangeDetectionStrategy} from '@angular/core';
+import {Component, ElementRef, DoCheck, ChangeDetectionStrategy, ChangeDetectorRef} from '@angular/core';
 import {kolorek} from "../kolorek";
 @Component({
   selector: 'app-child1',
   template: `
     <a>1</a>
+    <button (click)="triggerDetection()">cd 1</button>
     <app-child2></app-child2>
   `,
   changeDetection: ChangeDetectionStrategy.Default
 })
 export class Child1Component implements DoCheck{
 
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef, private cd: ChangeDetectorRef) {}
 
   ngDoCheck() {
     kolorek(this.el);
     console.log('CD - Child1Component');
+  }
+
+  triggerDetection() {
+    this.cd.detectChanges();
   }
 }
